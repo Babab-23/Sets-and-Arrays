be36ad4bf0088bbf191a7a88d187d3bf197aaa8b
Adrian <you@example.com>
2025-02-11 20:39:27 +0100

    second commit

---
+++ main2.py
@@ -0,0 +1,8 @@
+setx={"green","blue","red"}
+sety={"blue","yellow","red","green"}
+print("Original Set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz=setx.intersection(sety)
+print(setz)
